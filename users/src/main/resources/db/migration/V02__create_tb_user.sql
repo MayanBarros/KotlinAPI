e7ea885c115c3ea2b7152c82aceed46d52e28791
Mayan Barros da Silva <mayanbarrosdasilva@gmail.com>
2022-06-17 13:26:31 -0300

    Migrations OK

--- users/src/main/resources/db/migration/V02__create_tb_user.sql
+++ users/src/main/resources/db/migration/V02__create_tb_user.sql
@@ -1,9 +1,10 @@
 CREATE TABLE tb_user (
   id_user BIGINT AUTO_INCREMENT NOT NULL,
-   name VARCHAR(255) NOT NULL,
+   name VARCHAR(100) NOT NULL,
    age INT NOT NULL,
-   email VARCHAR(255) NOT NULL,
-   password VARCHAR(255) NOT NULL,
+   phone_number VARCHAR(20) NOT NULL,
+   email VARCHAR(150) NOT NULL,
+   password VARCHAR(100) NOT NULL,
    is_active BIT(1) NOT NULL,
    CONSTRAINT pk_tb_user PRIMARY KEY (id_user)
 );
--- users/src/main/resources/db/migration/V03__create_tb_address.sql
+++ users/src/main/resources/db/migration/V03__create_tb_address.sql
@@ -1,10 +1,8 @@
 CREATE TABLE tb_address (
   id_address BIGINT AUTO_INCREMENT NOT NULL,
    address VARCHAR(255) NOT NULL,
-   number VARCHAR(255) NOT NULL,
-   zip_postal VARCHAR(255) NOT NULL,
-   country VARCHAR(255) NOT NULL,
-   state VARCHAR(255) NOT NULL,
-   city VARCHAR(255) NOT NULL,
+   number VARCHAR(50) NOT NULL,
+   city VARCHAR(50) NOT NULL,
+   state VARCHAR(50) NOT NULL,
    CONSTRAINT pk_tb_address PRIMARY KEY (id_address)
 );
