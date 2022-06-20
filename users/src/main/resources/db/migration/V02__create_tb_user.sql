34168a2053d8fa4d6bd3ec1639fc0291cdf103e0
Mayan Barros da Silva <mayanbarrosdasilva@gmail.com>
2022-06-20 14:30:54 -0300

    adjusting size of password to 255

--- users/src/main/resources/db/migration/V02__create_tb_user.sql
+++ users/src/main/resources/db/migration/V02__create_tb_user.sql
@@ -4,7 +4,7 @@ CREATE TABLE tb_user (
    age INT NOT NULL,
    phone_number VARCHAR(20) NOT NULL,
    email VARCHAR(150) NOT NULL,
-   password VARCHAR(100) NOT NULL,
+   password VARCHAR(255) NOT NULL,
    is_active BIT(1) NOT NULL,
    CONSTRAINT pk_tb_user PRIMARY KEY (id_user)
 );
