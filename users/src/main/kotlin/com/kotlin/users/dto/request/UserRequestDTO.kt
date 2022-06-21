9a5939132f0f0394d428757708c57a686dfc3e8e
Mayan Barros da Silva <mayanbarrosdasilva@gmail.com>
2022-06-21 14:01:28 -0300

    adding attribute phoneNumber and method mapper toEntity()

--- users/src/main/kotlin/com/kotlin/users/dto/request/UserRequestDTO.kt
+++ users/src/main/kotlin/com/kotlin/users/dto/request/UserRequestDTO.kt
@@ -1,13 +1,8 @@
 package com.kotlin.users.dto.request
 
-import com.kotlin.users.entities.Address
+import com.kotlin.users.entities.User
 import javax.validation.Valid
-import javax.validation.constraints.Min
-import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotEmpty
-import javax.validation.constraints.NotNull
-import javax.validation.constraints.Positive
-import javax.validation.constraints.Size
+import javax.validation.constraints.*
 
 class UserRequestDTO (
 
@@ -22,6 +17,12 @@ class UserRequestDTO (
     @Positive
     val age: Int,
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Size(min = 12)
+    val phoneNumber: String,
+
     @NotBlank
     @NotEmpty
     @Size(min = 12, max = 150)
@@ -34,5 +35,23 @@ class UserRequestDTO (
     val password: String,
 
     @NotNull
-    val isActive: Boolean
+    val isActive: Boolean,
+
+    @Valid
+    @NotNull
+    val address: Set<AddressRequestDTO>
+    ) {
+
+    fun toEntity(): User {
+        return User(
+            id = null,
+            name = this.name,
+            age = this.age,
+            phoneNumber = this.phoneNumber,
+            email = this.email,
+            password = this.password,
+            isActive = this.isActive,
+            address = this.address.map { it.toEntity() }
         )
+    }
+}
