2b5a4407b032d6b9953e2d058923af9340057bcb
Mayan Barros da Silva <mayanbarrosdasilva@gmail.com>
2022-06-21 10:42:24 -0300

    Swagger OK

--- users/build.gradle.kts
+++ users/build.gradle.kts
@@ -27,7 +27,11 @@ dependencies {
 	implementation("org.flywaydb:flyway-mysql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//	implementation("io.springfox:springfox-boot-starter")
+
+	// Swagger
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
 	compileOnly("org.mapstruct:mapstruct:$mapstructVersion")
 	kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
 	runtimeOnly("mysql:mysql-connector-java")
