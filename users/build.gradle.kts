352a3ef341478369bda49363a586dfc34116e7c6
Mayan Barros da Silva <mayanbarrosdasilva@gmail.com>
2022-06-22 16:19:58 -0300

    dependencies Gson and JWT

--- users/build.gradle.kts
+++ users/build.gradle.kts
@@ -27,10 +27,15 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	// Gson
+	implementation("com.google.code.gson:gson:2.9.0")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+
 	// Swagger
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
-
 
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
