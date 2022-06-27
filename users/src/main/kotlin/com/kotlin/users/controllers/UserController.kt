dfacb90b44d7e7b1d11b3533f8c8c6dd7dbe5b0c
Mayan Barros da Silva <mayanbarrosdasilva@gmail.com>
2022-06-27 09:51:04 -0300

    refactoring

--- users/src/main/kotlin/com/kotlin/users/controllers/UserController.kt
+++ users/src/main/kotlin/com/kotlin/users/controllers/UserController.kt
@@ -1,6 +1,7 @@
 package com.kotlin.users.controllers;
 
 import com.kotlin.users.dto.request.UserRequestDTO
+import com.kotlin.users.dto.response.UserResponse
 import com.kotlin.users.entities.User
 import com.kotlin.users.services.UserService
 import org.springframework.http.HttpStatus
@@ -15,28 +16,34 @@ class UserController (
 ){
 
     @GetMapping
-    fun getAllUsers() = ResponseEntity.ok(userService.getAll())
+    fun getAllUsers() = userService.getAll()
+        .map { UserResponse.from(it) }
+        .let { ResponseEntity.ok().body(it) }
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun saveUser(@RequestBody @Valid requestUser: UserRequestDTO) {
-        userService.save(requestUser.toEntity())
+    fun saveUser(@RequestBody @Valid requestUser: UserRequestDTO): UserResponse {
+        return userService.save(requestUser.toEntity())
+            .let { UserResponse.from(it) }
     }
 
     @GetMapping("{id}")
-    fun getUserById(@PathVariable id: Long): ResponseEntity<User> {
-        return ResponseEntity.ok(userService.findById(id))
+    @ResponseStatus(HttpStatus.OK)
+    fun getUserById(@PathVariable id: Long): UserResponse? {
+        return userService.findById(id)
+            .let { UserResponse.from(it!!) }
     }
 
     @PatchMapping("{id}/deactivate")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deactivateUser(@PathVariable id: Long) {
-        ResponseEntity.ok(userService.deactivateUser(id))
+        userService.deactivateUser(id)
     }
 
     @PatchMapping("{id}/activate")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun activeUser(@PathVariable id: Long) {
-        ResponseEntity.ok(userService.activateUser(id))
+        userService.activateUser(id)
     }
 }
--- users/src/main/resources/db/migration/V04__create_tb_user_address.sql
+++ users/src/main/resources/db/migration/V04__create_tb_user_address.sql
@@ -5,7 +5,4 @@ CREATE TABLE `tb_user_address` (
 	INDEX `FKn70vncd23dcrg526sdgxxpc3a` (`user_id_user`) USING BTREE,
 	CONSTRAINT `FKjtu10ameq9gmab6t2k5icq5j4` FOREIGN KEY (`address_id_address`) REFERENCES `kotlin_api`.`tb_address` (`id_address`) ON UPDATE NO ACTION ON DELETE NO ACTION,
 	CONSTRAINT `FKn70vncd23dcrg526sdgxxpc3a` FOREIGN KEY (`user_id_user`) REFERENCES `kotlin_api`.`tb_user` (`id_user`) ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-COLLATE='utf8mb4_0900_ai_ci'
-ENGINE=InnoDB
-;
+);
